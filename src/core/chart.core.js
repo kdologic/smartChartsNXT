e29b7b7e61ce4976d9193ce4f2c4e93ad1c32be1
Kausik Dey <kausik.dey@learningmate.com>
2017-07-12 12:39:25 +0530

    Introduce coordinate chart

--- src/core/chart.core.js
+++ src/core/chart.core.js
@@ -8,18 +8,16 @@
 
 "use strict";
 
-let AreaChart = require("./../areaChart/areaChart"); 
+let AreaChart = require("./../coordinateChart/areaChart/areaChart"); 
 
 const preLoaderImg = "<svg width='135' height='140' viewBox='0 0 135 140' xmlns='http://www.w3.org/2000/svg' fill='#555'> <rect y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.5s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.5s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='30' y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.25s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.25s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='60' width='15' height='140' rx='6'> <animate attributeName='height' begin='0s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='90' y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.25s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.25s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='120' y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.5s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.5s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect></svg>";
 
 class Core {
     constructor() {
-        
         this.nameSpaceReadyStatus = false;
         this.AreaChart = AreaChart; 
         this.initCore();
     }
-
 
     initCore() {
         let self = this;
@@ -48,7 +46,12 @@ class Core {
             if (self.nameSpaceReadyStatus) {
                 clearInterval(statusCheck);
                 if (typeof successBack === "function") {
+                    var startTime = window.performance.now();
+
                     successBack.call(self);
+
+                    let endTitme = window.performance.now();
+                    console.log("Time elapsed for chart: %c" + (endTitme-startTime) + " Ms", "color:green");
                 }
             }
         }, 100);
