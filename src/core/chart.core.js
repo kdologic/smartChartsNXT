770103b4da70fc2367c4eb76f5d9c5b14b94e1bb
Kausik Dey <kausik.dey@learningmate.com>
2017-09-22 11:09:41 +0530

    Load fonts through google webFont Loader

--- src/core/chart.core.js
+++ src/core/chart.core.js
@@ -9,55 +9,56 @@
 "use strict";
 
 let polyfills = require("./../shims/polyfills");
-let AreaChart = require("./../charts/areaChart/areaChart"); 
-let PieChart = require("./../charts/pieChart/pieChart"); 
+let WebFonts = require("./../plugIns/webFontsLoader");
+let AreaChart = require("./../charts/areaChart/areaChart");
+let PieChart = require("./../charts/pieChart/pieChart");
 
 const preLoaderImg = "<svg width='135' height='140' viewBox='0 0 135 140' xmlns='http://www.w3.org/2000/svg' fill='#555'> <rect y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.5s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.5s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='30' y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.25s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.25s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='60' width='15' height='140' rx='6'> <animate attributeName='height' begin='0s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='90' y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.25s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.25s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect> <rect x='120' y='10' width='15' height='120' rx='6'> <animate attributeName='height' begin='0.5s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite' /> <animate attributeName='y' begin='0.5s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite' /> </rect></svg>";
 
 class Core {
     constructor() {
         this.nameSpaceReadyStatus = false;
-        this.AreaChart = AreaChart; 
-        this.PieChart = PieChart; 
+        this.AreaChart = AreaChart;
+        this.PieChart = PieChart;
         this.initCore();
     }
 
     initCore() {
-        let self = this;
-        self.addFont(function (event) {
-            self.nameSpaceReadyStatus = true;
+        this.addFont((event) => {
+            this.nameSpaceReadyStatus = true;
         });
     } /*End initLib()*/
 
     addFont(cb) {
-        let fontLink = document.createElement("link");
-        fontLink.href = "https://fonts.googleapis.com/css?family=Lato:400,700";
-        fontLink.rel = "stylesheet";
-        document.getElementsByTagName("head")[0].appendChild(fontLink);
-        if (typeof cb === "function") {
-            fontLink.addEventListener('load', function (e) {
-                cb(e);
-            }, false);
-        }
+        WebFonts.load({
+            google: {
+                families: ['Lato:400,700']
+            },
+            /* Called when all of the web fonts have either finished loading or failed to load, as long as at least one loaded successfully. */
+            active: function () {
+                if (typeof cb === "function") {
+                    cb();
+                }
+            }
+        });
     } /*End addFont()*/
 
     ready(successBack) {
-        let self = this;
         /* strat polling for the ready state*/
-        let statusCheck = setInterval(function () {
-            if (self.nameSpaceReadyStatus) {
+        let statusCheck = setInterval(() => {
+            if (this.nameSpaceReadyStatus) {
                 clearInterval(statusCheck);
                 if (typeof successBack === "function") {
                     var startTime = window.performance.now();
 
-                    successBack.call(self);
+                    successBack.call(this);
 
                     let endTitme = window.performance.now();
-                    console.log("Time elapsed for chart: %c" + (endTitme-startTime) + " Ms", "color:green");
+                    console.log("Time elapsed for chart: %c" + (endTitme - startTime) + " Ms", "color:green");
                 }
             }
         }, 100);
     } /*End ready()*/
 }
 
-module.exports = Core; 
+module.exports = Core;
