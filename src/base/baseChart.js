6ed3bf30c9b05996c123437579cbc135be48b545
Kausik Dey <kausik.dey@learningmate.com>
2017-10-10 19:00:24 +0530

    Added Error screen

--- src/base/baseChart.js
+++ src/base/baseChart.js
@@ -22,26 +22,32 @@ let transformer = require("./../core/transformer");
 let animator = require("./../plugIns/animator");
 
 class BaseChart {
-    constructor(chartType, opts) {
-        this.util = new UtilCore();
-        this.geom = new GeomCore();
-        this.event = new EventCore();
-        this.ui = new UiCore();
-        this.transformer = transformer;
-        this.plugins = {
-            animator: animator
-        };
-        this.chartType = chartType;
-        this.CHART_OPTIONS = this.util.extends(opts, {});
-        this.CHART_DATA = {};
-        this.CHART_CONST = {
-            FIX_WIDTH: 800,
-            FIX_HEIGHT: 600,
-            MIN_WIDTH: 250,
-            MIN_HEIGHT: 400
-        };
-        this.runId = chartType + "_" + this.util.uuidv4();
-        this.timeOut = null;
+    constructor(opts) {
+        try {
+            this.util = new UtilCore();
+            this.geom = new GeomCore();
+            this.event = new EventCore();
+            this.ui = new UiCore();
+            this.transformer = transformer;
+            this.plugins = {
+                animator: animator
+            };
+            this.chartType = opts.type;
+            this.CHART_OPTIONS = this.util.extends(opts, {});
+            this.CHART_DATA = {};
+            this.CHART_CONST = {
+                FIX_WIDTH: 800,
+                FIX_HEIGHT: 600,
+                MIN_WIDTH: 250,
+                MIN_HEIGHT: 400
+            };
+            this.runId = this.chartType + "_" + this.util.uuidv4();
+            this.timeOut = null;
+        } catch (ex) {
+            ex.errorIn = `Error in ${opts.type} base constructor : ${ex.message}`;
+            this.showErrorScreen(opts, ex, ex.errorIn);
+            throw ex;
+        }
     }
 
     initBase() {
@@ -76,7 +82,6 @@ class BaseChart {
         this.event.dispatchEvent(onInitEvent);
 
         let strSVG = "<svg xmlns:svg='http:\/\/www.w3.org\/2000\/svg' xmlns='http:\/\/www.w3.org\/2000\/svg' xmlns:xlink='http:\/\/www.w3.org\/1999\/xlink'" +
-            //"viewBox='0 0 " + this.CHART_CONST.FIX_WIDTH + " " + this.CHART_CONST.FIX_HEIGHT + "'" +
             "version='1.1'" +
             "width='" + this.CHART_OPTIONS.width + "'" +
             "height='" + this.CHART_OPTIONS.height + "'" +
@@ -130,8 +135,8 @@ class BaseChart {
                         }, 100);
                     } else {
                         self.timeOut = setTimeout(() => {
-                            if(typeof callBackInit === "function"){
-                                callBackInit.call(self); 
+                            if (typeof callBackInit === "function") {
+                                callBackInit.call(self);
                             }
                         }, 500);
                     }
@@ -150,6 +155,74 @@ class BaseChart {
 
     getRunId(chartType) {
         return this.runId;
+    }
+
+    showErrorScreen(opts, ex, mgs) {
+        let container = document.querySelector("#" + opts.targetElem);
+        let width = container.offsetWidth;
+        let height = container.offsetHeight;
+
+        let strSVG = "<svg xmlns:svg='http:\/\/www.w3.org\/2000\/svg' xmlns='http:\/\/www.w3.org\/2000\/svg' xmlns:xlink='http:\/\/www.w3.org\/1999\/xlink'" +
+            "version='1.1'" +
+            "width='" + width + "'" +
+            "height='" + height + "'" +
+            "id='" + opts.type + "-error'" +
+            "style='background:#eee;-moz-tap-highlight-color: rgba(0, 0, 0, 0);-webkit-tap-highlight-color:rgba(0, 0, 0, 0);-webkit-user-select:none;-khtml-user-select: none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none;user-select:none;'" +
+            "> <\/svg>";
+
+        container.innerHTML = "";
+        container.insertAdjacentHTML("beforeend", strSVG);
+        let shadowId = this.ui.dropShadow(opts.type + "-error");
+        let svgContainer = container.querySelector("#" + opts.type + "-error");
+        let upperBoxPath = [
+            "M", 0, 0,
+            "H", width,
+            "V", 50
+        ];
+        let lowerBoxPath = [
+            "M", 0, height,
+            "H", width,
+            "v", -40
+        ];
+        let zigzagPath = [];
+        for (let i = width, counter = 0; i >= 0; i -= 10, counter++) {
+            zigzagPath.push("l", -10, (counter % 2 === 0 ? -10 : 10));
+        }
+        zigzagPath.push("Z");
+        upperBoxPath.push.apply(upperBoxPath, zigzagPath);
+        lowerBoxPath.push.apply(lowerBoxPath, zigzagPath);
+        strSVG = "<path id='upperBox' d='" + upperBoxPath.join(" ") + "' filter = '" + shadowId + "' fill='rgba(244, 67, 54, 0.16)' stroke='#333' stroke-width='0' opacity='1' pointer-events='none'></path>";
+        strSVG += "<path id='lowerBox' d='" + lowerBoxPath.join(" ") + "' filter = '" + shadowId + "' fill='rgba(244, 67, 54, 0.16)' stroke='#333' stroke-width='0' opacity='1' pointer-events='none'></path>";
+        svgContainer.insertAdjacentHTML("beforeend", strSVG);
+
+        strSVG = "<circle cx='" + (width / 2) + "' cy='" + (height / 2 - 80) + "' r='25' fill='#717171'/>";
+        strSVG += "<text id='errorTextGroup' fill='#fff' x='" + (width / 2 - 5) + "' y='" + ((height / 2) - 70) + "' font-weight='bold' font-size='35' >i<\/text>";
+        strSVG += "<g>";
+        strSVG += "  <text id='errorTextGroup' fill='#717171' font-family='Lato' >";
+        strSVG += "    <tspan class='err-text' id='errtxt1' x='" + 0 + "' y='" + height / 2 + "' font-size='26'>Oops! Something went wrong. <\/tspan>";
+        strSVG += "    <tspan class='err-text' id='errtxt2' x='" + 0 + "' y='" + ((height / 2) + 30) + "' font-size='16'>See the javascript console for technical details.<\/tspan>";
+        strSVG += "  <\/text>";
+        strSVG += "<\/g>";
+
+        svgContainer.insertAdjacentHTML("beforeend", strSVG);
+        let arrErrText = svgContainer.querySelectorAll("#errorTextGroup .err-text");
+        (function adjustFontSize() {
+            let overFlow = false;
+            for (let j = 0; j < arrErrText.length; j++) {
+                let eTextLen = arrErrText[j].getComputedTextLength();
+                arrErrText[j].setAttribute("x", (width - eTextLen) / 2);
+                if (eTextLen > width) {
+                    overFlow = true;
+                    break;
+                }
+            }
+            if (overFlow) {
+                for (let j = 0; j < arrErrText.length; j++) {
+                    arrErrText[j].setAttribute("font-size", arrErrText[j].getAttribute("font-size") - 1);
+                }
+                adjustFontSize();
+            }
+        })();
     }
 }
 
